66ed44ef9037ae3a346371a559f09c224d6da7ec
Jose Navarro <jose.navarro@famoco.com>
2017-01-03 18:18:26 +0100

    removed dependency and console.log

--- src/components/MapDrawer/index.js
+++ src/components/MapDrawer/index.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import Map, { Marker, GoogleApiWrapper } from 'google-maps-react'
-import styled from 'styled-components';
+import Map, { Marker, GoogleApiWrapper } from 'google-maps-react';
 
 class MapDrawer extends Component {
   render() {
     const { width, height, marker, google, loaded } = this.props;
-    console.log(marker)
     return (
       <div className={this.props.className}>
         { loaded ?
@@ -14,8 +12,8 @@ class MapDrawer extends Component {
             containerStyle={{ width: width, height: height }}
             centerAroundCurrentLocation={true}
           >
-          {marker && <Marker name={marker.get('name')} position={{ lat: marker.get('lat'), lng: marker.get('lng') }} />}
-        </Map>
+            {marker && <Marker name={marker.get('name')} position={{ lat: marker.get('lat'), lng: marker.get('lng') }} />}
+          </Map>
           :
           <div>LOADING</div>
         }
